9bf5a1db5eee83322fe5f17be094ef5002b3df58
laltu <laltu.lspl@gmail.com>
2024-04-11 19:54:18 +0530

    init

---
+++ src/components/Table/TableCell.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+import { useTableCellClasses } from './useTableCellClasses'
+
+const { tableCellClasses } = useTableCellClasses()
+</script>
+<template>
+    <td :class="tableCellClasses">
+        <slot></slot>
+    </td>
+</template>
---
+++ src/components/Table/TableRow.vue
@@ -0,0 +1,10 @@
+<template>
+  <tr :class="tableRowClasses">
+    <slot></slot>
+  </tr>
+</template>
+<script lang="ts" setup>
+import { useTableRowClasses } from './Table/useTableRowClasses'
+
+const { tableRowClasses } = useTableRowClasses()
+</script>
